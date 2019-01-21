3985c3e702b37ca93c18e10204a30ea9663d0b29
Ahmad <ahmad.abu.esmaeel985@gmail.com>
2019-01-21 02:23:03 +0200

    get template from web and make it similar to our project

--- index.php
+++ index.php
@@ -1,3 +1,321 @@
-<?php
-echo "welcome to films demo";
-?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<title>Videos Demo</title>
+
+		<!-- Google font -->
+		<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:700%7CNunito:300,600" rel="stylesheet"> 
+
+		<!-- Bootstrap -->
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+
+		<!-- Font Awesome Icon -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<!-- Custom stlylesheet -->
+		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+
+		
+
+    </head>
+	<body>
+
+		<!-- Header -->
+		<header id="header">
+			<!-- Nav -->
+			<div id="nav">
+				<!-- Main Nav -->
+				<div id="nav-fixed">
+					<div class="container">
+						<!-- logo -->
+						<div class="nav-logo">
+							<a href="index.php" class="logo"><img src="./img/logo.png" alt=""></a>
+						</div>
+						<!-- /logo -->
+
+						<!-- nav -->
+						<ul class="nav-menu nav navbar-nav">
+							<li><a href="index.php">Most</a></li>
+							<li><a href="index.php">Recent</a></li>
+							<li class="cat-1"><a href="index.php">Images</a></li>
+							<li class="cat-2"><a href="index.php">Videos</a></li>
+							<li class="cat-3"><a href="index.php">Messages</a></li>
+							<li class="cat-4"><a href="index.php">Sports</a></li>
+						</ul>
+						<!-- /nav -->
+
+						<!-- search -->
+						<div class="nav-btns">
+							<button class="search-btn"><i class="fa fa-search"></i></button>
+							<div class="search-form">
+								<input class="search-input" type="text" name="search" placeholder="Enter Your Search ...">
+								<button class="search-close"><i class="fa fa-times"></i></button>
+							</div>
+						</div>
+						<!-- /search  -->
+					</div>
+				</div>
+				<!-- /Main Nav -->
+
+				<!-- Aside Nav -->
+				<div id="nav-aside">
+					<!-- nav -->
+					<div class="section-row">
+						<ul class="nav-aside-menu">
+							<li><a href="index.html">Home</a></li>
+							<li><a href="about.html">About Us</a></li>
+							<li><a href="#">Join Us</a></li>
+							<li><a href="#">Advertisement</a></li>
+							<li><a href="contact.html">Contacts</a></li>
+						</ul>
+					</div>
+					<!-- /nav -->
+
+					<!-- widget posts -->
+					<div class="section-row">
+						<h3>Recent Posts</h3>
+						<div class="post post-widget">
+							<a class="post-img" href="blog-post.html"><img src="./img/widget-2.jpg" alt=""></a>
+							<div class="post-body">
+								<h3 class="post-title"><a href="blog-post.html">Pagedraw UI Builder Turns Your Website Design Mockup Into Code Automatically</a></h3>
+							</div>
+						</div>
+
+						<div class="post post-widget">
+							<a class="post-img" href="blog-post.html"><img src="./img/widget-3.jpg" alt=""></a>
+							<div class="post-body">
+								<h3 class="post-title"><a href="blog-post.html">Why Node.js Is The Coolest Kid On The Backend Development Block!</a></h3>
+							</div>
+						</div>
+
+						<div class="post post-widget">
+							<a class="post-img" href="blog-post.html"><img src="./img/widget-4.jpg" alt=""></a>
+							<div class="post-body">
+								<h3 class="post-title"><a href="blog-post.html">Tell-A-Tool: Guide To Web Design And Development Tools</a></h3>
+							</div>
+						</div>
+					</div>
+					<!-- /widget posts -->
+
+					<!-- social links -->
+					<div class="section-row">
+						<h3>Follow us</h3>
+						<ul class="nav-aside-social">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+							<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+						</ul>
+					</div>
+					<!-- /social links -->
+
+					<!-- aside nav close -->
+					<button class="nav-aside-close"><i class="fa fa-times"></i></button>
+					<!-- /aside nav close -->
+				</div>
+				<!-- Aside Nav -->
+			</div>
+			<!-- /Nav -->
+		</header>
+		<!-- /Header -->
+
+		<!-- section -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+						<div class="section-title">
+							<h2>Videos</h2>
+						</div>
+					</div>
+
+					<!-- post -->
+					<div class="col-md-4">
+						<div class="post">
+							<a class="post-img" href="blog-post.html"><img src="./img/post-3.jpg" alt=""></a>
+							<div class="post-body">
+								<div class="post-meta">
+									<a class="post-category cat-1" href="category.html">Web Design</a>
+									<span class="post-date">March 27, 2018</span>
+								</div>
+								<h3 class="post-title"><a href="blog-post.html">Pagedraw UI Builder Turns Your Website Design Mockup Into Code Automatically</a></h3>
+							</div>
+						</div>
+					</div>
+					<!-- /post -->
+
+					<!-- post -->
+					<div class="col-md-4">
+						<div class="post">
+							<a class="post-img" href="blog-post.html"><img src="./img/post-4.jpg" alt=""></a>
+							<div class="post-body">
+								<div class="post-meta">
+									<a class="post-category cat-2" href="category.html">JavaScript</a>
+									<span class="post-date">March 27, 2018</span>
+								</div>
+								<h3 class="post-title"><a href="blog-post.html">Chrome Extension Protects Against JavaScript-Based CPU Side-Channel Attacks</a></h3>
+							</div>
+						</div>
+					</div>
+					<!-- /post -->
+
+					<!-- post -->
+					<div class="col-md-4">
+						<div class="post">
+							<a class="post-img" href="blog-post.html"><img src="./img/post-5.jpg" alt=""></a>
+							<div class="post-body">
+								<div class="post-meta">
+									<a class="post-category cat-3" href="category.html">Jquery</a>
+									<span class="post-date">March 27, 2018</span>
+								</div>
+								<h3 class="post-title"><a href="blog-post.html">Ask HN: Does Anybody Still Use JQuery?</a></h3>
+							</div>
+						</div>
+					</div>
+					<!-- /post -->
+
+					<div class="clearfix visible-md visible-lg"></div>
+
+					<!-- post -->
+					<div class="col-md-4">
+						<div class="post">
+							<a class="post-img" href="blog-post.html"><img src="./img/post-6.jpg" alt=""></a>
+							<div class="post-body">
+								<div class="post-meta">
+									<a class="post-category cat-2" href="category.html">JavaScript</a>
+									<span class="post-date">March 27, 2018</span>
+								</div>
+								<h3 class="post-title"><a href="blog-post.html">Why Node.js Is The Coolest Kid On The Backend Development Block!</a></h3>
+							</div>
+						</div>
+					</div>
+					<!-- /post -->
+
+					<!-- post -->
+					<div class="col-md-4">
+						<div class="post">
+							<a class="post-img" href="blog-post.html"><img src="./img/post-1.jpg" alt=""></a>
+							<div class="post-body">
+								<div class="post-meta">
+									<a class="post-category cat-4" href="category.html">Css</a>
+									<span class="post-date">March 27, 2018</span>
+								</div>
+								<h3 class="post-title"><a href="blog-post.html">CSS Float: A Tutorial</a></h3>
+							</div>
+						</div>
+					</div>
+					<!-- /post -->
+
+					<!-- post -->
+					<div class="col-md-4">
+						<div class="post">
+							<a class="post-img" href="blog-post.html"><img src="./img/post-2.jpg" alt=""></a>
+							<div class="post-body">
+								<div class="post-meta">
+									<a class="post-category cat-1" href="category.html">Web Design</a>
+									<span class="post-date">March 27, 2018</span>
+								</div>
+								<h3 class="post-title"><a href="blog-post.html">Tell-A-Tool: Guide To Web Design And Development Tools</a></h3>
+							</div>
+						</div>
+					</div>
+					<!-- /post -->
+				</div>
+				<!-- /row -->
+
+			
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /section -->
+		
+	
+		
+		<!-- Footer -->
+		<footer id="footer">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-5">
+						<div class="footer-widget">
+							<div class="footer-logo">
+								<a href="index.html" class="logo"><img src="./img/logo.png" alt=""></a>
+							</div>
+							<ul class="footer-nav">
+								<li><a href="#">Privacy Policy</a></li>
+								<li><a href="#">Advertisement</a></li>
+							</ul>
+							<div class="footer-copyright">
+								<span>&copy; <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved 
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></span>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-4">
+						<div class="row">
+							<div class="col-md-6">
+								<div class="footer-widget">
+									<h3 class="footer-title">About Us</h3>
+									<ul class="footer-links">
+										<li><a href="#">About Us</a></li>
+										<li><a href="#">Join Us</a></li>
+										<li><a href="#">Contacts</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="footer-widget">
+									<h3 class="footer-title">Catagories</h3>
+									<ul class="footer-links">
+										<li><a href="#">Videos</a></li>
+										<li><a href="#">Images</a></li>
+										<li><a href="#">Messages</a></li>
+										<li><a href="#">Sports</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<div class="footer-widget">
+							<h3 class="footer-title">Join our Newsletter</h3>
+							<div class="footer-newsletter">
+								<form>
+									<input class="input" type="email" name="newsletter" placeholder="Enter your email">
+									<button class="newsletter-btn"><i class="fa fa-paper-plane"></i></button>
+								</form>
+							</div>
+							<ul class="footer-social">
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+								<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+							</ul>
+						</div>
+					</div>
+
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</footer>
+		<!-- /Footer -->
+
+		<!-- jQuery Plugins -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/main.js"></script>
+
+	</body>
+</html>
